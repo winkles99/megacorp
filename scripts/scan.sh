db382c38942bfd8dedf5ff4555babb1f889137d9
winkles99 <kiah.hurt@outlook.com>
2025-12-15 03:16:05 -0600

    K: modified scripts.sh

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,1 +1,13 @@
-# TODO: write the script
+printf "\n====== SCANNING FOR CREDIT CARD NUMBERS ======\n"
+grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "========= CREDIT CARD SCAN COMPLETE =========="
+
+printf "\n========= SCANNING FOR PHONE NUMBERS =========\n"
+grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
+grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
+echo "========= PHONE NUMBER SCAN COMPLETE ========="
+
+printf "\n========= SCANNING FOR PHONE NUMBERS =========\n"
+grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
+grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
+echo "========= PHONE NUMBER SCAN COMPLETE ========="
